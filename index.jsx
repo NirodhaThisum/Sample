fd8a54cc564a8ec5628cd9e003b37ba2e317d09e
Nirodha Thisum <thisumnirodha@gmail.com>
2024-08-05 16:08:58 +0530

    Create name form

--- index.jsx
+++ index.jsx
@@ -4,6 +4,10 @@ const Home = () => {
     return ( <>
         <h1>This is the first line</h1>
         <p>First paragaraph</p>
+        <form>
+            <label for="name">Enter your name</label>
+            <input type="text" placeholder='Enter yor name' id="name"></input>
+        </form>
     </> );
 }
  
